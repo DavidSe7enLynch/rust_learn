ccf295497029e7edb964aa03c010223db4c2fa28
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-02-21 11:04:26 -0500

    first

---
+++ target/debug/deps/plot-d1d16bfd6fdb5f7d.d
@@ -0,0 +1,5 @@
+/Users/ruironghuang/study/self_study/rust/plot/target/debug/deps/plot-d1d16bfd6fdb5f7d.rmeta: src/main.rs
+
+/Users/ruironghuang/study/self_study/rust/plot/target/debug/deps/plot-d1d16bfd6fdb5f7d.d: src/main.rs
+
+src/main.rs:
