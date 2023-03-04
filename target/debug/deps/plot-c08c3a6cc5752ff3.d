23327410c810ad5ed53ce3fcb371b7862a337e65
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-04 13:30:43 -0500

    delete target

--- target/debug/deps/plot-c08c3a6cc5752ff3.d
+++
@@ -1,5 +0,0 @@
-/Users/ruironghuang/study/self_study/rust/plot/target/debug/deps/plot-c08c3a6cc5752ff3.rmeta: src/main.rs
-
-/Users/ruironghuang/study/self_study/rust/plot/target/debug/deps/plot-c08c3a6cc5752ff3.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/plot-d1d16bfd6fdb5f7d.d
+++
@@ -1,5 +0,0 @@
-/Users/ruironghuang/study/self_study/rust/plot/target/debug/deps/plot-d1d16bfd6fdb5f7d.rmeta: src/main.rs
-
-/Users/ruironghuang/study/self_study/rust/plot/target/debug/deps/plot-d1d16bfd6fdb5f7d.d: src/main.rs
-
-src/main.rs:
