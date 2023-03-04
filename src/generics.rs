ed5a76df96e02f7cf8c6804abdeb9064e9299fce
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-04 14:40:57 -0500

    trait

--- src/generics.rs
+++ src/generics.rs
@@ -1,9 +1,12 @@
 use std::cmp::{max, Ord};
 use std::hash::{Hash, Hasher};
+use std::ops::Add;
+
+mod traits;
 
 pub struct Point<T>
 where
-    T: Ord + Hash,
+    T: Ord + Hash + Add,
 {
     x: T,
     y: T,
@@ -11,7 +14,7 @@ where
 
 impl<T> Point<T>
 where
-    T: Ord + Hash,
+    T: Ord + Hash + Add,
 {
     pub fn new(x: T, y: T) -> Point<T> {
         Point { x, y }
@@ -30,12 +33,20 @@ where
     }
 }
 
-impl<T: Hash> Hash for Point<T>
+impl<T> Hash for Point<T>
 where
-    T: Ord + Hash,
+    T: Ord + Hash + Add,
 {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.x.hash(state);
         self.y.hash(state);
+    }
+}
+
+impl<T> traits::Norm2d<T> for Point<T> 
+where T: Ord + Hash + Add
+{
+    fn len(&self) -> T {
+        self.x + self.y
     }
 }
---
+++ src/generics/traits.rs
@@ -0,0 +1,3 @@
+pub trait Norm2d<T> {
+    fn len(&self) -> T;
+}
