fdcfb610c1adea74d9299365de83826c460b3f24
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-05 17:53:01 -0500

    combining

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use env_logger::Builder;
+use log::{error, info};
+mod simple_tests;
+mod generics;
+
+
+fn lib() {
+    // Initialize logger
+    Builder::new().parse_filters("debug").init();
+    // simple_tests::hashmap_test();
+    // simple_tests::log_test();
+    simple_tests::generics_test();
+    info!("Hello, world!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-use env_logger::Builder;
-use log::{error, info};
-
-mod generics;
-mod simple_tests;
+mod lib;
+// mod generics;
+// mod simple_tests;
 
 fn main() {
-    // Initialize logger
-    Builder::new().parse_filters("debug").init();
-    // simple_tests::hashmap_test();
-    // simple_tests::log_test();
-    simple_tests::generics_test();
-    info!("Hello, world!");
+    lib();
 }
