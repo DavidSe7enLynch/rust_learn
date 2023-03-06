ddbf578575f328a5b866f9f78ab0ffb0a6ae2d20
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-06 16:40:54 -0500

    Rc<RefCell<>>

--- src/boxs/linked_list.rs
+++ src/boxs/linked_list.rs
@@ -1,18 +1,26 @@
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc};
 
 use log::info;
 use List::{Cons, Nil};
 
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
 pub fn list_test() {
-    let a = Rc::new(Cons(1, Rc::new(Cons(0, Rc::new(Nil)))));
+    let val = Rc::new(RefCell::new(0));
+    let a = Rc::new(Cons(
+        Rc::new(RefCell::new(1)),
+        Rc::new(Cons(Rc::clone(&val), Rc::new(Nil))),
+    ));
     info!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(2, Rc::clone(&a));
+    let b = Cons(Rc::new(RefCell::new(2)), Rc::clone(&a));
     info!("count after creating a = {}", Rc::strong_count(&a));
-    let c = Cons(3, Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
     info!("count after creating a = {}", Rc::strong_count(&a));
+
+    *val.borrow_mut() += 10;
+    info!("muted: a: {:?}, b: {:?}, c: {:?}", a, b, c);
 }
--- src/boxs/refcells/test.rs
+++ src/boxs/refcells/test.rs
@@ -14,7 +14,9 @@ impl MessageSender for MockMessageSender {
 
 #[test]
 fn limit_checker() {
-    let mock_msg_sender = MockMessageSender { msgs_sent: RefCell::new(vec![]) };
+    let mock_msg_sender = MockMessageSender {
+        msgs_sent: RefCell::new(vec![]),
+    };
     let mut limit_checker = LimitChecker::new(&mock_msg_sender, 22);
     limit_checker.set_value(20);
     assert_eq!(mock_msg_sender.msgs_sent.borrow().len(), 1);
