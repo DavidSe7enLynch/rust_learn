0ee68f42ea530523db7ffc32c6611752c58a9295
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-06 16:23:22 -0500

    borrow

--- src/boxs/refcells/test.rs
+++ src/boxs/refcells/test.rs
@@ -1,21 +1,21 @@
-use log::debug;
+use std::cell::RefCell;
 
 use super::*;
 
 struct MockMessageSender {
-    msgs_sent: Vec<String>,
+    msgs_sent: RefCell<Vec<String>>,
 }
 
 impl MessageSender for MockMessageSender {
     fn send(&self, msg: &str) {
-        self.msgs_sent.push(msg.to_string());
+        self.msgs_sent.borrow_mut().push(msg.to_string());
     }
 }
 
 #[test]
 fn limit_checker() {
-    let mock_msg_sender = MockMessageSender { msgs_sent: vec![] };
+    let mock_msg_sender = MockMessageSender { msgs_sent: RefCell::new(vec![]) };
     let mut limit_checker = LimitChecker::new(&mock_msg_sender, 22);
     limit_checker.set_value(20);
-    assert_eq!(mock_msg_sender.msgs_sent.len(), 1);
+    assert_eq!(mock_msg_sender.msgs_sent.borrow().len(), 1);
 }
