9551064554b62f23aaefde79983ced06a784b4e8
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-06 14:44:18 -0500

    deref

--- src/boxs.rs
+++ src/boxs.rs
@@ -8,3 +8,16 @@ enum List {
 fn list_test() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Nil))));
 }
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn mybox_test() {
+    let mybox = MyBox::new(1);
+    assert_eq!(1, *mybox);
+}
