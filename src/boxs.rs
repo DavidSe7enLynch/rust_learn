1e693bb5782ba07868cd31f978cf3227f8fae663
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-06 14:58:55 -0500

    mods

--- src/boxs.rs
+++ src/boxs.rs
@@ -1,23 +1,5 @@
-use List::{Cons, Nil};
+mod linked_list;
+mod deref;
+mod drops;
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
 
-fn list_test() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Nil))));
-}
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
-}
-
-fn mybox_test() {
-    let mybox = MyBox::new(1);
-    assert_eq!(1, *mybox);
-}
