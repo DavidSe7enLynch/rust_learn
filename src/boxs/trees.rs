1bf628772fc32ac6829ab91c27bfd005c4eef42e
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-06 17:15:57 -0500

    weak

--- src/boxs/trees.rs
+++ src/boxs/trees.rs
@@ -1,10 +1,24 @@
+use std::borrow::BorrowMut;
 use std::cell::RefCell;
-use std::rc::Rc;
-use std::sync::Weak;
+use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
 struct Node {
     value: i32,
     parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn tree_test() {
+    let leaf = Rc::new(Node {
+        value: 10,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+    let branch = Rc::new(Node {
+        value: 20,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
 }
