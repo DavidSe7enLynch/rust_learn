277142af0656911c35e7848238e90d0e0b98f6ba
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-07 00:44:44 -0500

    err

--- src/threads.rs
+++ src/threads.rs
@@ -1,4 +1,5 @@
 use log::info;
+use std::any::Any;
 use std::error::Error;
 use std::sync::mpsc::{self, SendError};
 use std::thread;
@@ -15,14 +16,15 @@ pub fn move_test() {
 pub fn channel_test() -> Result<(), Box<dyn Error>> {
     let (tx, rx) = mpsc::channel();
 
+    // let h = thread::spawn(move || -> Result<(), &str> {
+    //     tx.send("hello world").map_err(|_| "send error")?;
+    //     Ok(())
+    // });
     let h = thread::spawn(move || {
-        tx.send("hello world").unwrap_or_else(|e| {
-            // return Err(e);
-        });
-        // Ok(())
+        tx.send("hello world").unwrap();
     });
 
-    // h.join().unwrap_or_else(|e| {return Err(e);});
+    h.join().map_err(|_| "join error")?;
     // thread::sleep(Duration::from_millis(1));
     info!("received: {}", rx.try_recv()?);
 
