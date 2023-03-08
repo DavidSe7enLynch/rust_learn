08ec1721d58f9f70b40eca3fd9ff0356e47942fd
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-08 14:03:23 -0500

    all

--- src/lib.rs
+++ src/lib.rs
@@ -14,28 +14,28 @@ mod macros;
 pub fn lib() {
     // Initialize logger
     Builder::new().parse_filters("debug").init();
-    // simple_tests::hashmap_test();
-    // simple_tests::log_test();
-    // simple_tests::generics_test();
-    // boxs::drops::drop_test();
-    // boxs::linked_list::list_test();
-    // threads::move_test();
-    // threads::channel_test().unwrap_or_else(|e| {
-    //     error!("{e}");
-    // });
-    // threads::mpsc_test().unwrap_or_else(|e| {
-    //     error!("{e}");
-    // });
+    simple_tests::hashmap_test();
+    simple_tests::log_test();
+    simple_tests::generics_test();
+    boxs::drops::drop_test();
+    boxs::linked_list::list_test();
+    threads::move_test();
+    threads::channel_test().unwrap_or_else(|e| {
+        error!("{e}");
+    });
+    threads::mpsc_test().unwrap_or_else(|e| {
+        error!("{e}");
+    });
     locks::lock_test().unwrap_or_else(|e| {
         error!("{e}");
     });
     locks::lock_thread_test().unwrap_or_else(|e| {
         error!("{e}");
     });
-    // patterns::pattern_test();
-    // unsafes::rawpointer();
-    // unsafes::slices();
-    // unsafes::extern_test();
+    patterns::pattern_test();
+    unsafes::rawpointer();
+    unsafes::slices();
+    unsafes::extern_test();
     macros::hrr_vec();
     info!("Hello, world!");
 }
