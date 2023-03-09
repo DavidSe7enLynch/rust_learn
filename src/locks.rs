34402d59981910b940c9b12da598b299bc8aec44
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-09 16:44:25 -0500

    thread err

--- src/locks.rs
+++ src/locks.rs
@@ -1,5 +1,4 @@
 use std::error::Error;
-
 use std::sync::{Arc, Mutex};
 use std::thread;
 
@@ -20,19 +19,22 @@ pub fn lock_test() -> Result<(), Box<dyn Error>> {
     Err("lock test error".into())
 }
 
-pub fn lock_thread_test() -> Result<(), Box<dyn Error>> {
+pub fn lock_thread_test() -> Result<(), String> {
     let counter = Arc::new(Mutex::new(0));
     let mut handlers = vec![];
     for _ in 0..10 {
         let c = Arc::clone(&counter);
-        let h = thread::spawn(move || {
-            let mut val = c.lock().unwrap();
+        let h = thread::spawn(move || -> Result<(), String> {
+            let mut val = c.lock().map_err(|e| e.to_string())?;
             *val += 1;
+            Ok(())
         });
         handlers.push(h);
     }
     for h in handlers {
-        h.join().map_err(|_| "join error")?;
+        h.join()
+            .map_err(|_| "join err")?
+            .map_err(|e| format!("thread err: {e}"))?;
     }
     info!("counter = {}", *counter.lock().unwrap());
     Ok(())
